4bb52a13c927dac13e6236743c9bd1fcc2659257
Sascha Strauß <snoopy1alpha@gmail.com>
2024-12-04 19:56:14 +0100

    feat(multidimarray) Dimensions can now reveal their size
    - Refactoring of day 4 solution. Passing the size is no longer needed

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,49 +1,53 @@
+import de.dreamcube.hornet_queen.array.PrimitiveCharArray
+
 fun main() {
     val testList = readInput("Day04_test")
     val inputList = readInput("Day04")
 
-    val (exInput, exSize) = readInput(testList)
-    val exResult = countXmas(exInput, exSize)
+    val exInput = readInput(testList)
+    val exResult = countXmas(exInput)
     println("Example result: $exResult")
-    val exResult2 = countMasX(exInput, exSize)
+    val exResult2 = countMasX(exInput)
     println("Example result2: $exResult2")
 
-    val (input, inputSize) = readInput(inputList)
-    val result = countXmas(input, inputSize)
+    val input = readInput(inputList)
+    val result = countXmas(input)
     println("Result: $result")
-    val result2 = countMasX(input, inputSize)
+    val result2 = countMasX(input)
     println("Result2: $result2")
     println()
 
 }
 
-fun countMasX(array: MultiDimArray<Char>, size: Int): Int {
+fun countXmas(array: PrimitiveMultiDimArray<Char>): Int {
     var sum = 0
-    for (j in 0 until size) {
-        for (i in 0 until size) {
-            val currentChar = array[j, i]
-            if (currentChar == 'A') {
-                sum += checkMasX(array, size, j, i)
-            }
-        }
-    }
-    return sum
-}
-
-fun countXmas(array: MultiDimArray<Char>, size: Int): Int {
-    var sum = 0
-    for (j in 0 until size) {
-        for (i in 0 until size) {
+    for (j in 0 until array.getDimensionSize(0)) {
+        for (i in 0 until array.getDimensionSize(1)) {
             val currentChar = array[j, i]
             if (currentChar == 'X') {
-                sum += checkXmas(array, size, j, i)
+                sum += checkXmas(array, j, i)
             }
         }
     }
     return sum
 }
 
-fun checkMasX(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
+fun countMasX(array: PrimitiveMultiDimArray<Char>): Int {
+    var sum = 0
+    for (j in 0 until array.getDimensionSize(0)) {
+        for (i in 0 until array.getDimensionSize(1)) {
+            val currentChar = array[j, i]
+            if (currentChar == 'A') {
+                sum += checkMasX(array, j, i)
+            }
+        }
+    }
+    return sum
+}
+
+fun checkMasX(array: PrimitiveMultiDimArray<Char>, j: Int, i: Int): Int {
+    val height = array.getDimensionSize(0)
+    val width = array.getDimensionSize(1)
     val checkWord = "MAS"
     val revCheckWord = "SAM"
     val value = array[j, i]
@@ -53,17 +57,19 @@ fun checkMasX(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     val first: String =
         (if (j - 1 < 0 || i - 1 < 0) "" else array[j - 1, i - 1].toString()) +
                 value.toString() +
-                (if (j + 1 >= size || i + 1 >= size) "" else array[j + 1, i + 1].toString())
-    val second: String = (if (j - 1 < 0 || i + 1 >= size) "" else array[j - 1, i + 1].toString()) +
+                (if (j + 1 >= height || i + 1 >= width) "" else array[j + 1, i + 1].toString())
+    val second: String = (if (j - 1 < 0 || i + 1 >= width) "" else array[j - 1, i + 1].toString()) +
             value.toString() +
-            (if (j + 1 >= size || i - 1 < 0) "" else array[j + 1, i - 1].toString())
+            (if (j + 1 >= height || i - 1 < 0) "" else array[j + 1, i - 1].toString())
     if ((first == checkWord || first == revCheckWord) && (second == checkWord || second == revCheckWord)) {
         return 1
     }
     return 0
 }
 
-fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
+fun checkXmas(array: PrimitiveMultiDimArray<Char>, j: Int, i: Int): Int {
+    val height = array.getDimensionSize(0)
+    val width = array.getDimensionSize(1)
     val checkWord = "XMAS"
     var sum = 0
     val value = array[j, i]
@@ -74,7 +80,7 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     // read right
     var word = ""
     for (x in i..i + 3) {
-        if (x >= size) {
+        if (x >= width) {
             break
         }
         word += array[j, x]
@@ -100,7 +106,7 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
 
     // read down
     for (y in j..j + 3) {
-        if (y >= size) {
+        if (y >= height) {
             break
         }
         word += array[y, i]
@@ -127,7 +133,7 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     // diagonals
     var y = j
     for (x in i..i + 3) {
-        if (x >= size || y >= size) {
+        if (x >= width || y >= height) {
             break
         }
         word += array[y, x]
@@ -140,7 +146,7 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     word = ""
     y = j
     for (x in i downTo i - 3) {
-        if (x < 0 || y >= size) {
+        if (x < 0 || y >= height) {
             break
         }
         word += array[y, x]
@@ -168,7 +174,7 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     y = j
 
     for (x in i..i + 3) {
-        if (x >= size || y < 0) {
+        if (x >= width || y < 0) {
             break
         }
         word += array[y, x]
@@ -181,19 +187,15 @@ fun checkXmas(array: MultiDimArray<Char>, size: Int, j: Int, i: Int): Int {
     return sum
 }
 
-fun readInput(input: List<String>): Pair<MultiDimArray<Char>, Int> {
-    var out: MultiDimArray<Char>? = null
-    var dim = 0
+fun readInput(input: List<String>): PrimitiveMultiDimArray<Char> {
+    val dim = input[0].length // I don't care about any exception here ... if empty -> bad luck
+    val out: PrimitiveMultiDimArray<Char> = PrimitiveMultiDimArray(dim, dim) { size -> PrimitiveCharArray(size) }
     var j = 0
     input.forEach { line ->
-        if (out == null) {
-            dim = line.length
-            out = MultiDimArray(dim, dim) { size -> Array(size) { ' ' } }
-        }
         for (i in line.indices) {
-            out!![j, i] = line[i]
+            out[j, i] = line[i]
         }
         j += 1
     }
-    return Pair(out!!, dim)
+    return out
 }
