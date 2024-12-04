c389a3543a4506b00057ee321cdb4f35a1867c35
strauss <strauss@users.noreply.github.com>
2024-12-04 18:55:51 +0100

    feat(aoc) Migrated solution for day 1
    - Added hornet-queen to dependency list.
    - Added solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,66 @@
+import de.dreamcube.hornet_queen.list.PrimitiveIntArrayList
+import de.dreamcube.hornet_queen.map.HashTableBasedMapBuilder
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Test results")
+    part1(testInput).println()
+    part2(testInput).println()
+    println()
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
+    println("Results")
     part1(input).println()
     part2(input).println()
 }
+
+private fun part1(input: List<String>): Int {
+    val (leftList, rightList) = parseInput(input)
+    val sorted1: MutableList<Int> = ArrayList(leftList)
+    sorted1.sort()
+    val sorted2: MutableList<Int> = ArrayList(rightList)
+    sorted2.sort()
+
+    val absDifferenceList = ArrayList<Int>()
+
+    val it1 = sorted1.iterator()
+    val it2 = sorted2.iterator()
+
+    while (it1.hasNext()) {
+        assert(it2.hasNext())
+        absDifferenceList.add(abs(it1.next() - it2.next()))
+    }
+
+    return absDifferenceList.sum()
+}
+
+private fun part2(input: List<String>): Int {
+    val (leftList, rightList) = parseInput(input)
+    val countMap: MutableMap<Int, Int> = HashTableBasedMapBuilder.useIntKey().useIntValue().create()
+    rightList.forEach {
+        countMap[it] = countMap.getOrZero(it) + 1
+    }
+    var similarityScore = 0
+    leftList.forEach {
+        similarityScore += it * countMap.getOrZero(it)
+    }
+
+    return similarityScore
+}
+
+private fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+    val leftList: MutableList<Int> = PrimitiveIntArrayList()
+    val rightList: MutableList<Int> = PrimitiveIntArrayList()
+    val splitPattern = Regex("\\s+")
+
+    for (line in input) {
+        val split = line.split(splitPattern)
+        leftList.add(split[0].toInt())
+        rightList.add(split[1].toInt())
+    }
+
+    return Pair(leftList, rightList)
+}
+
+private fun Map<Int, Int>.getOrZero(key: Int) = this[key] ?: 0
