052901e8286c2ed08388eafeb2ff83fee56ef17f
Sascha Strauß <snoopy1alpha@gmail.com>
2024-12-14 23:24:46 +0100

    feat(utils) Some additional functions and refactoring

--- src/aoc_util/Utils.kt
+++ src/aoc_util/Utils.kt
@@ -1,6 +1,8 @@
 package aoc_util
 
 import de.dreamcube.hornet_queen.array.PrimitiveCharArray
+import de.dreamcube.hornet_queen.list.PrimitiveCharArrayList
+import de.dreamcube.hornet_queen.list.PrimitiveIntArrayList
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -38,4 +40,52 @@ fun parseInputAsMultiDimArray(input: List<String>): PrimitiveMultiDimArray<Char>
         j += 1
     }
     return out
+}
+
+val intNumberRegex = "(-?\\d+)".toRegex()
+fun String.extractInts(): List<Int> {
+    val out = PrimitiveIntArrayList()
+    intNumberRegex.findAll(this).iterator().forEach { matchResult ->
+        out.add(matchResult.groupValues[1].toInt())
+    }
+    return out
+}
+
+fun getAdjacentValues(array: PrimitiveMultiDimArray<Char>, row: Int, col: Int): List<Char> {
+    val result = PrimitiveCharArrayList()
+
+    result.addIfInBounds(array, row - 1, col) // north
+    result.addIfInBounds(array, row - 1, col + 1) // north-east
+    result.addIfInBounds(array, row, col + 1) // east
+    result.addIfInBounds(array, row + 1, col + 1) // south-east
+    result.addIfInBounds(array, row + 1, col) // south
+    result.addIfInBounds(array, row + 1, col - 1) // south-west
+    result.addIfInBounds(array, row, col - 1) // west
+    result.addIfInBounds(array, row - 1, col - 1) // north-west
+
+    return result
+}
+
+fun MutableList<Char>.addIfInBounds(
+    from: PrimitiveMultiDimArray<Char>,
+    row: Int,
+    col: Int,
+    filter: (Char) -> Boolean = { true }
+) {
+    val height = from.getDimensionSize(0)
+    val width = from.getDimensionSize(1)
+    if (inBounds(row, col, height, width)) {
+        val element = from[row, col]
+        if (filter(element)) {
+            this.add(element)
+        }
+    }
+}
+
+fun inBounds(position: Pair<Int, Int>, height: Int, width: Int): Boolean {
+    return inBounds(position.first, position.second, height, width)
+}
+
+fun inBounds(y: Int, x: Int, height: Int, width: Int): Boolean {
+    return y in 0..<height && x in 0..<width
 }
