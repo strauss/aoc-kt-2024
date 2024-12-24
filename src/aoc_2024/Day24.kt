678fb3003649be2a06e24a50eddbe2be359d3385
strauss <strauss@users.noreply.github.com>
2024-12-25 00:31:27 +0100

    feat(2024) Solution 24 path to part 2 ... but not there yet...

--- src/aoc_2024/Day24.kt
+++ src/aoc_2024/Day24.kt
@@ -6,6 +6,13 @@ import de.dreamcube.hornet_queen.map.HashTableBasedMapBuilder
 import java.util.*
 
 fun main() {
+//    solve()
+    val input = readInput2024("Day24")
+    val (graph, assignment) = createDirectedGraph(input)
+    playAround(graph, assignment)
+}
+
+private fun solve() {
     val testInput = readInput2024("Day24_test")
     val (testGraph, testAssignment) = createDirectedGraph(testInput)
     val testResult = evaluate(testGraph, testAssignment)
@@ -15,7 +22,178 @@ fun main() {
     val (graph, assignment) = createDirectedGraph(input)
     val result = evaluate(graph, assignment)
     println("Result: $result")
-    println(graph.getEdges().size)
+}
+
+private fun playAround(graph: BitSetAdjacencyBasedGraph<Operation>, assignment: Map<String, Int>) {
+    val correctPositions = determineCorrectPositions(graph, assignment)
+    println("Correct positions: $correctPositions")
+    val possibleIncorrectVertices: Set<Operation> = getPossibleIncorrectVertexIds(graph, correctPositions)
+    println("Possible vertices causing an incorrect result: ${possibleIncorrectVertices.size}")
+
+    val outputToOperationMap = mutableMapOf<String, Operation>()
+    graph.run {
+        for (v in this) {
+            outputToOperationMap[v.t] = v
+        }
+    }
+
+    for (position in 0..44) {
+        val z = "z${position.toString().padStart(2, '0')}"
+        println(getFormulaFor(outputToOperationMap, z))
+    }
+
+    // test swap
+//    val newGraph = swapOutputConnections(graph, "z12", "z15")
+//    val newCorrectPositions = determineCorrectPositions(newGraph, assignment)
+//    println("New correct positions: $newCorrectPositions")
+//    val newPossibleIncorrectVertices: Set<Operation> = getPossibleIncorrectVertexIds(newGraph, newCorrectPositions)
+//    println("New possible vertices causing an incorrect result: ${newPossibleIncorrectVertices.size}")
+}
+
+private fun getFormulaFor(operations: Map<String, Operation>, output: String): String {
+    val operation = operations[output] ?: return ""
+    val inputRegex = "[xy]\\d{2}".toRegex()
+    var formula = operation.formula()
+
+    if (!inputRegex.matches(operation.s1)) {
+        val s1Formula = getFormulaFor(operations, operation.s1)
+        formula = formula.replace(operation.s1, s1Formula)
+    }
+    if (!inputRegex.matches(operation.s2)) {
+        val s2Formula = getFormulaFor(operations, operation.s2)
+        formula = formula.replace(operation.s2, s2Formula)
+    }
+    return "[$output=$formula]"
+
+}
+
+private fun swapOutputConnections(graph: BitSetAdjacencyBasedGraph<Operation>, first: String, second: String): BitSetAdjacencyBasedGraph<Operation> {
+    val copyGraph = graph.createCopy()
+    // find vertices
+    var firstVertex: Operation? = null
+    var secondVertex: Operation? = null
+    copyGraph.run {
+        for (v in this) {
+            if (firstVertex != null && secondVertex != null) {
+                break
+            }
+            if (v.t == first) {
+                firstVertex = v
+            } else if (v.t == second) {
+                secondVertex = v
+            }
+        }
+    }
+
+    if (firstVertex != null && secondVertex != null) {
+        val oldFirst: Operation = firstVertex!!
+        val oldSecond: Operation = secondVertex!!
+        val firstIn = mutableListOf<Operation>()
+        val firstOut = mutableListOf<Operation>()
+        val secondIn = mutableListOf<Operation>()
+        val secondOut = mutableListOf<Operation>()
+        copyGraph.run {
+            for (v in oldFirst.backwardAdjacencies()) {
+                firstIn.add(v)
+            }
+            for (v in oldFirst.adjacencies()) {
+                firstOut.add(v)
+            }
+            for (v in oldSecond.backwardAdjacencies()) {
+                secondIn.add(v)
+            }
+            for (v in oldSecond.adjacencies()) {
+                secondOut.add(v)
+            }
+        }
+        println("First in: $firstIn")
+        println("First out: $firstOut")
+        println("Second in: $secondIn")
+        println("Second out: $secondOut")
+        val newFirst = Operation(oldFirst.op, oldFirst.s1, oldFirst.s2, second)
+        val newSecond = Operation(oldSecond.op, oldSecond.s1, oldSecond.s2, first)
+        // reestablish the connections
+        copyGraph.run {
+            oldFirst.remove()
+            oldSecond.remove()
+            introduceVertex(newFirst)
+            introduceVertex(newSecond)
+            // sustain incoming edges
+            for (v in firstIn) {
+                v.connect(newFirst)
+            }
+            for (v in secondIn) {
+                v.connect(newSecond)
+            }
+            // swap outgoing edges
+            for (v in firstOut) {
+                newSecond.connect(v)
+            }
+            for (v in secondOut) {
+                newFirst.connect(v)
+            }
+        }
+    }
+
+    return copyGraph
+}
+
+private fun getPossibleIncorrectVertexIds(
+    graph: BitSetAdjacencyBasedGraph<Operation>,
+    correctPositions: LinkedHashSet<String>
+): Set<Operation> {
+    val correctOutputVertices: MutableList<Operation> = mutableListOf()
+    val possibleIncorrectVertices: MutableSet<Operation> = mutableSetOf()
+    val incorrectPositions = mutableListOf<String>()
+    graph.run {
+        for (v in this) {
+            if (v.t in correctPositions) {
+                correctOutputVertices.add(v)
+            } else {
+                possibleIncorrectVertices.add(v)
+                if (v.t.startsWith("z")) {
+                    incorrectPositions.add(v.t)
+                }
+            }
+        }
+        for (w in correctOutputVertices) {
+            for (u in w.backwardAdjacencies()) {
+                possibleIncorrectVertices.remove(u)
+            }
+        }
+    }
+    incorrectPositions.sort()
+    println("Incorrect positions: $incorrectPositions")
+    println("Graph size: ${graph.size}")
+    println("Output vertices with correct result: ${correctOutputVertices.size}")
+    return possibleIncorrectVertices
+}
+
+private fun determineCorrectPositions(
+    graph: BitSetAdjacencyBasedGraph<Operation>,
+    assignment: Map<String, Int>
+): LinkedHashSet<String> {
+    val xKeys: List<String> = assignment.keys.asSequence().filter { it.startsWith("x") }.sorted().toList().reversed()
+    val yKeys: List<String> = assignment.keys.asSequence().filter { it.startsWith("y") }.sorted().toList().reversed()
+    val x: Long = assembleLongFromBits(xKeys, assignment)
+    val y: Long = assembleLongFromBits(yKeys, assignment)
+    var correctResult = x + y
+    var result: Long = evaluate(graph, assignment)
+    println(correctResult)
+    println(result)
+
+    var position = 0
+    val correctPositions = LinkedHashSet<String>()
+    while (correctResult != 0L && result != 0L) {
+        val currentPosition = "z${position.toString().padStart(2, '0')}"
+        if (correctResult and 1 == result and 1) {
+            correctPositions.add(currentPosition)
+        }
+        correctResult = correctResult ushr 1
+        result = result ushr 1
+        position += 1
+    }
+    return correctPositions
 }
 
 private fun evaluate(graph: BitSetAdjacencyBasedGraph<Operation>, assignment: Map<String, Int>): Long {
@@ -26,15 +204,13 @@ private fun evaluate(graph: BitSetAdjacencyBasedGraph<Operation>, assignment: Ma
         results.putAll(assignment)
 
         // Process in topological order (with the help of my dear friend Kahn-Knuth)
-        // get inDegree for all vertices ... unfortunately my structure can only get the outDegree easily ... so we perform a search for that
         val inDegree = HashTableBasedMapBuilder.useIntKey().useIntValue().create()
-        val inDegreeVisitor = object : BitSetAdjacencyBasedGraph<Operation>.SearchVisitor() {
-            override fun visitEdge(edge: BitSetAdjacencyBasedGraph<Operation>.Edge, from: Operation, to: Operation) {
-                val toIndegree = inDegree[to.getId()] ?: 0
-                inDegree[to.getId()] = toIndegree + 1
+        for (v in this) {
+            val vInDegree = v.getInDegree()
+            if (vInDegree > 0) {
+                inDegree[v.getId()] = vInDegree
             }
         }
-        search(BitSetAdjacencyBasedGraph.SearchType.BFS, inDegreeVisitor)
 
         val nextCandidates: Queue<Operation> = LinkedList()
         for (v in this) {
@@ -70,14 +246,21 @@ private fun evaluate(graph: BitSetAdjacencyBasedGraph<Operation>, assignment: Ma
 
         // evaluate the result
         val finalResultKeys: List<String> = results.keys.asSequence().filter { it.startsWith("z") }.sorted().toList().reversed()
-        var result = 0L
-        for (key in finalResultKeys) {
-            val current = results[key]!!
-            result = result shl 1
-            result = result xor current.toLong()
-        }
-        result
+        assembleLongFromBits(finalResultKeys, results)
     }
+}
+
+private fun assembleLongFromBits(
+    keys: List<String>,
+    assignments: Map<String, Int>
+): Long {
+    var result = 0L
+    for (key in keys) {
+        val current = assignments[key]!!
+        result = result shl 1
+        result = result xor current.toLong()
+    }
+    return result
 }
 
 private fun createDirectedGraph(input: List<String>): Pair<BitSetAdjacencyBasedGraph<Operation>, Map<String, Int>> {
@@ -137,6 +320,8 @@ private data class Operation(val op: Operator, val s1: String, val s2: String, v
         }
         return result and 1 // limit result to last bit
     }
+
+    fun formula(): String = "($s1 $op $s2)"
 }
 
 private enum class Operator {
