378dbf2e9ab92a3d99cb14fe9a97c60a0d72d3c1
strauss <strauss@users.noreply.github.com>
2025-12-14 01:34:09 +0100

    feat(2023) Day 10 Part 1

--- src/aoc_2023/Day10.kt
+++ src/aoc_2023/Day10.kt
@@ -1,22 +1,125 @@
 package aoc_2023
 
+import aoc_util.Primitive2DCharArray
 import aoc_util.graph.BitSetAdjacencyBasedGraph
 import aoc_util.readInput2023
 import aoc_util.solve
 
+private const val connectNorth = "|LJ"
+private const val connectEast = "-LF"
+private const val connectSouth = "|7F"
+private const val connectWest = "-J7"
+
 fun main() {
     val testLines = readInput2023("Day10_test")
-    val (testGraph, tsId) = parse(testLines)
-    solve("Test result", testGraph) {
-        part1(it, tsId)
-    }
+//    val (testGraph, tsId) = parseAsGraph(testLines)
+//    solve("Test result", testGraph) {
+//        part1(it, tsId)
+//    }
+    val testArray = parseAsArray(testLines)
+    solve("Test result", testArray, ::solve1)
 
     val lines = readInput2023("Day10")
-    val (graph, sId) = parse(lines)
-    solve("Result", graph) {
-        part1(it, sId)
+//    val (graph, sId) = parseAsGraph(lines)
+//    solve("Result", graph) {
+//        part1(it, sId)
+//    }
+    val array = parseAsArray(lines)
+    solve("Result", array, ::solve1)
+
+}
+
+private fun solve1(array: Primitive2DCharArray): Int {
+    // search the S
+    val start = findStart(array)
+    val (rowS, colS) = start
+    val depth = HashMap<Pair<Int, Int>, Int>()
+//    val visited = Array(array.height) { BitSet(array.width) }
+    val currentDepth = 0
+    depth[start] = currentDepth
+
+    val q = ArrayDeque<Pair<Int, Int>>()
+
+    // check north
+    val rowNorth = rowS - 1
+    if (rowNorth >= 0 && array[rowNorth, colS] in connectSouth) {
+        val north = rowNorth to colS
+        depth[north] = 1
+        q.addLast(north)
+    }
+    // check east
+    val colEast = colS + 1
+    if (colEast < array.width && array[rowS, colEast] in connectWest) {
+        val east = rowS to colEast
+        depth[east] = 1
+        q.addLast(east)
+    }
+    // check south
+    val rowSouth = rowS + 1
+    if (rowSouth < array.height && array[rowSouth, colS] in connectNorth) {
+        val south = rowSouth to colS
+        depth[south] = 1
+        q.addLast(south)
+    }
+    // check west
+    val colWest = colS - 1
+    if (colWest >= 0 && array[rowS, colWest] in connectEast) {
+        val west = rowS to colWest
+        depth[west] = 1
+        q.addLast(west)
     }
 
+    var maxDepth = 0
+
+    // start search
+    while (q.isNotEmpty()) {
+        val current = q.removeFirst()
+        val currentDepth = depth[current]!!
+        maxDepth = maxDepth.coerceAtLeast(currentDepth)
+        val (row, col) = current
+        val currentChar = array[row, col]
+        if (currentChar in connectNorth) {
+            val north = (row - 1) to col
+            if (depth[north] == null) {
+                depth[north] = currentDepth + 1
+                q.addLast(north)
+            }
+        }
+        if (currentChar in connectEast) {
+            val east = row to col + 1
+            if (depth[east] == null) {
+                depth[east] = currentDepth + 1
+                q.addLast(east)
+            }
+        }
+        if (currentChar in connectSouth) {
+            val south = row + 1 to col
+            if (depth[south] == null) {
+                depth[south] = currentDepth + 1
+                q.addLast(south)
+            }
+        }
+        if (currentChar in connectWest) {
+            val west = row to col - 1
+            if (depth[west] == null) {
+                depth[west] = currentDepth + 1
+                q.addLast(west)
+            }
+        }
+    }
+
+    return maxDepth
+}
+
+private fun findStart(array: Primitive2DCharArray): Pair<Int, Int> {
+    for (row in 0..<array.height) {
+        for (col in 0..<array.width) {
+            if (array[row, col] == 'S') {
+                return row to col
+            }
+        }
+    }
+    return -1 to -1
 }
 
 private fun part1(graph: BitSetAdjacencyBasedGraph<V>, startId: Int): Int {
@@ -30,9 +133,11 @@ private fun part1(graph: BitSetAdjacencyBasedGraph<V>, startId: Int): Int {
     }
 }
 
+private fun parseAsArray(lines: List<String>) = Primitive2DCharArray.parseFromLines(lines, '.')
+
 private data class V(val row: Int, val col: Int, val c: Char)
 
-private fun parse(lines: List<String>): Pair<BitSetAdjacencyBasedGraph<V>, Int> {
+private fun parseAsGraph(lines: List<String>): Pair<BitSetAdjacencyBasedGraph<V>, Int> {
     val width = lines.asSequence().map { it.length }.max()
     val graph = BitSetAdjacencyBasedGraph<V>(directed = false)
     // create vertices and place them in array
@@ -46,10 +151,6 @@ private fun parse(lines: List<String>): Pair<BitSetAdjacencyBasedGraph<V>, Int> 
         }
     }
     // materialize edges
-    val connectNorth = "|LJ"
-    val connectEast = "-LF"
-    val connectSouth = "|7F"
-    val connectWest = "-J7"
     var startId: Int = -1
     graph.run {
         var start: V? = null
