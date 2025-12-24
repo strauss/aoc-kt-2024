9420d44cc46ff7d4a8c832ae924f64e2bb17cc40
strauss <strauss@users.noreply.github.com>
2025-12-24 13:59:38 +0100

    feat(2023) Day 17 broken A* with Dijkstra as heuristic (not working yet)

--- src/aoc_2023/Day17.kt
+++ src/aoc_2023/Day17.kt
@@ -13,7 +13,7 @@ fun main() {
 
     val lines = readInput2023("Day17")
     val input = Primitive2DCharArray.parseFromLines(lines)
-    solve("Result", input, ::searchOptimalHeatLoss)
+//    solve("Result", input, ::searchOptimalHeatLoss)
 
 }
 
@@ -30,6 +30,98 @@ private fun searchOptimalHeatLoss(
     start: LavaSearchState = LavaSearchState(0, 0, 0, Direction.EAST),
     goal: Coordinate = Coordinate(array.height - 1, array.width - 1)
 ): Int {
+    // we perform a backwards Dijkstra for calculating the heuristic for a follow-up A*
+    val backwardsStart = LavaSearchState(goal.row, goal.col, 0, Direction.NORTH)
+    val heuristic = performDijkstra(array, backwardsStart)
+
+    // now we do A* with the precalculated heuristic
+    val marker = HashSet<LavaSearchState>()
+    val buffer = PriorityQueue<Pair<LavaSearchState, Int>>(Comparator.comparing { it.second })
+    val cost = HashMap<Coordinate, Int>()
+    val parent = HashMap<LavaSearchState, LavaSearchState>()
+
+    // prepare start state
+    val startCoordinate = Coordinate(start.row, start.col)
+    cost[startCoordinate] = 0
+    buffer.offer(start to (heuristic[startCoordinate] ?: 0))
+
+    // prepare target found
+    var target: LavaSearchState? = null
+
+    while (buffer.isNotEmpty()) {
+        val (currentState, _) = buffer.poll()
+        val currentCoordinate = Coordinate(currentState.row, currentState.col)
+        if (currentState in marker) {
+            continue
+        }
+        marker.add(currentState)
+        // check if we reached the goal
+        if (goal == currentCoordinate) {
+            target = currentState
+            break
+        }
+
+        // follow-up states
+        val currentDir = currentState.dir
+        val currentStraight = currentState.straight
+        val costSoFar = cost[currentCoordinate]!! // if this is null, the algorithm is broken
+
+        for (op: Operation in Operation.entries) {
+            val (nextDir, nextStraight) = nextDirAndStraight(currentDir, currentStraight, op)
+            val (dRow, dCol) = nextDir.delta
+            val nextRow = currentState.row + dRow
+            val nextCol = currentState.col + dCol
+            if (nextStraight <= 3 &&
+                nextRow in 0..<array.height && nextCol in 0..<array.width
+            ) { // only if next state is in bounds
+                val costKey = Coordinate(nextRow, nextCol)
+                val nextCost = costSoFar + array[nextRow, nextCol].digitToInt()
+                if (nextCost < (cost[costKey] ?: Integer.MAX_VALUE)) {
+                    cost[costKey] = nextCost
+                    val nextState = LavaSearchState(nextRow, nextCol, nextStraight, nextDir)
+                    parent[nextState] = currentState
+                    buffer.offer(nextState to nextCost + (heuristic[costKey] ?: 0))
+                }
+            }
+        }
+    }
+
+    // traceback
+    var current = target
+    val path = ArrayDeque<LavaSearchState>()
+    while (current != null) {
+        path.addFirst(current)
+        current = parent[current]
+    }
+
+    fun determineOperation(prevDir: Direction, curDir: Direction): String {
+        return when {
+            prevDir == curDir -> Operation.FORWARD.toString()
+            prevDir.rotateLeft() == curDir -> Operation.LEFT.toString()
+            prevDir.rotateRight() == curDir -> Operation.RIGHT.toString()
+            else -> "FORBIDDEN"
+        }
+    }
+
+    // print
+    var previous: LavaSearchState? = null
+    for (state in path) {
+        print("$state")
+        if (previous == null) {
+            println()
+        } else {
+            println(determineOperation(previous.dir, state.dir))
+        }
+        previous = state
+    }
+
+    return cost[goal] ?: -1
+}
+
+private fun performDijkstra(
+    array: Primitive2DCharArray,
+    startState: LavaSearchState
+): HashMap<Coordinate, Int> {
     // Initialize search stuff
     val cost = HashMap<Coordinate, Int>()
     val visited = HashSet<LavaSearchState>()
@@ -37,44 +129,28 @@ private fun searchOptimalHeatLoss(
     val buffer = PriorityQueue<Pair<LavaSearchState, Int>>(Comparator.comparing { it.second })
 
     // handle start state
-    cost[Coordinate(start.row, start.col)] = 0
-    buffer.offer(start to 0)
-
-    var foundGoal: LavaSearchState? = null
+    cost[Coordinate(startState.row, startState.col)] = 0
+    buffer.offer(startState to 0)
 
     fun createAndAddNextState(currentState: LavaSearchState, op: Operation, costSoFar: Int) {
         val (row, col, straight, dir) = currentState
-        val nextDir = when (op) {
-            Operation.FORWARD -> dir
-            Operation.LEFT -> dir.rotateLeft()
-            Operation.RIGHT -> dir.rotateRight()
-        }
-        val nextStraight = when (op) {
-            Operation.LEFT, Operation.RIGHT -> 1
-            Operation.FORWARD -> straight + 1
-        }
+        val (nextDir, nextStraight) = nextDirAndStraight(dir, straight, op)
         val (dRow, dCol) = nextDir.delta
         val nextRow = row + dRow
         val nextCol = col + dCol
-        if (
-            nextStraight <= 3 && // TODO: here something is wrong ... maybe Dijkstra is not the way to go after all :-( or maybe we need a graph
-            nextRow in 0..<array.height && nextCol in 0..<array.width
-        ) { // only if next state is in bounds
+        if (nextRow in 0..<array.height && nextCol in 0..<array.width) { // only if next state is in bounds
             val costKey = Coordinate(nextRow, nextCol)
             val nextCost = costSoFar + array[nextRow, nextCol].digitToInt()
             if (nextCost < (cost[costKey] ?: Integer.MAX_VALUE)) {
+                cost[costKey] = nextCost
                 val nextState = LavaSearchState(nextRow, nextCol, nextStraight, nextDir)
                 parent[nextState] = currentState
-                cost[costKey] = nextCost
                 buffer.offer(nextState to nextCost)
-                if (goal.row == nextState.row && goal.col == nextState.col) {
-                    foundGoal = currentState
-                }
             }
         }
     }
 
-
+    // the actual search
     while (buffer.isNotEmpty()) {
         val (currentState, _) = buffer.poll()
         val currentCoordinate = Coordinate(currentState.row, currentState.col)
@@ -87,23 +163,31 @@ private fun searchOptimalHeatLoss(
         }
     }
 
-    var current = foundGoal
-    val path = ArrayDeque<LavaSearchState>()
-    while (current != null) {
-        path.addFirst(current)
-        current = parent[current]
+    return cost
+}
+
+private fun nextDirAndStraight(
+    currentDir: Direction,
+    currentStraight: Int,
+    op: Operation
+): Pair<Direction, Int> {
+    val nextDir = when (op) {
+        Operation.FORWARD -> currentDir
+        Operation.LEFT -> currentDir.rotateLeft()
+        Operation.RIGHT -> currentDir.rotateRight()
     }
-
-    path.toList().forEach { println(it) }
-
-    return cost[goal] ?: -1
+    val nextStraight = when (op) {
+        Operation.LEFT, Operation.RIGHT -> 1
+        Operation.FORWARD -> currentStraight + 1
+    }
+    return Pair(nextDir, nextStraight)
 }
 
 private data class LavaSearchState(
     val row: Int,
     val col: Int,
     val straight: Int,
-    val direction: Direction
+    val dir: Direction
 )
 
 private enum class Operation {
